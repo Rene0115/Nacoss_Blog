82e976d6f04ba72f73cc82ae6be0d8fecbec7900
Rene0115 <enere0115@gmail.com>
2022-12-26 23:06:39 +0100

    user signup final fix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,28 +3,17 @@
 /* eslint-disable import/no-duplicates */
 /* eslint-disable import/extensions */
 import express from "express";
-import multer from 'multer';
 import userController from "../controllers/user.controller.js";
 import validator from "../validators/validator.js";
 import validateUserSignInSchema from "../validators/signin.validator.js";
 import authentication from "../middlewares/auth.middlewares.js";
-
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename(req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-const upload = multer({ storage });
+import store from "../config/multer.config.js";
 
 const userRouter = express.Router();
-userRouter.post("/signup", [upload.single('photo')], userController.create);
+userRouter.post("/signup", [store.single('image')], userController.create);
 userRouter.post("/login", [validator(validateUserSignInSchema)], userController.loginUser);
 userRouter.get("/verified-user", userController.getVerifiedUser);
-userRouter.put("/image", authentication, upload.single('photo'), userController.updateUserPhoto);
+userRouter.put("/image", authentication, store.single('photo'), userController.updateUserPhoto);
 
 userRouter.get('/verify/:token', userController.verify);
 
