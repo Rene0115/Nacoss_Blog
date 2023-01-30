1bcf52b2a49f21c18f073e182b3b7288d6b96a9b
Rene0115 <enere0115@gmail.com>
2023-01-30 14:41:53 +0100

    added comment routes

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -1,10 +1,15 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-cycle */
 import express from 'express';
 import commentController from '../controllers/comment.controller.js';
 import validator from '../validators/validator.js';
 import authentication from '../middlewares/auth.middlewares.js';
+import createCommentValidator, { commentIdValidator, updateCommentValidator } from '../validators/comment.validator.js';
 
 const commentRouter = express.Router();
 
-commentRouter.post('/createcomment', [authentication, validator()],commentController.createComment);
+commentRouter.post('/createcomment', [authentication, validator(createCommentValidator)], commentController.createComment);
+commentRouter.put('/updatecomment', [authentication, validator(updateCommentValidator)], commentController.editComment);
+commentRouter.delete('/deletecomment', [authentication, validator(commentIdValidator)], commentController.deleteComment);
 
 export default commentRouter;
