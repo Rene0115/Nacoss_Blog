9c396f2da287e09fab655509af36fefb55be2e60
Rene0115 <enere0115@gmail.com>
2023-02-11 20:24:29 +0100

    cron job finished

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import cron from 'node-cron';
 import middleware from './middlewares/middlewares.js';
 import 'express-async-errors';
 import userServices from './services/user.services.js';
+import { mailGenerator, transporter } from './config/mailer.config.js';
 
 dotenv.config();
 const app = express();
@@ -15,22 +16,37 @@ const logger = pino();
 
 middleware(app);
 
-// const cronJob = async () => {
-//   try {
-//     cron.schedule('* * * * *', async () => {
-//       const users = await userServices.getAllUsers();
-//       for (let i = 0; i < users.length; i++) {
-//         if (users) {
+const cronJob = async () => {
+  try {
+    cron.schedule('* * 24 * *', async () => {
+      const users = await userServices.getAllUsers();
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].verified === false) {
+          const response = {
+            body: {
+              name: `${users[i].username}`,
+              intro: 'Remember to Verify Your email.',
+              outre: 'Your account would be autodeleted in a month.'
+            }
+          };
+          const mail = mailGenerator.generate(response);
+          const message = {
+            from: 'Nacoss Blog <enere0115@gmail.com>',
+            to: users[i].email,
+            subject: 'Password reset success',
+            html: mail
+          };
 
-//         }
-//       }
-//     });
-//   } catch (err) {
-//     logger.error(err);
-//   }
-// };
+          transporter.sendMail(message);
+        }
+      }
+    });
+  } catch (err) {
+    logger.error(err);
+  }
+};
 
-// cronJob();
+cronJob();
 
 app.listen(process.env.PORT, () => {
   let port = process.env.PORT;
